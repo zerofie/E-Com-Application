6f593ffeeb62dc450cd053c9d5bd96047f04b244
Aditya <adityakaul05@gmail.com>
2026-09-10 13:46:38 +0530

    till update user

--- src/main/java/com/app/ecom/User.java
+++ src/main/java/com/app/ecom/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,28 +12,13 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-//@Entity (name= "user_table")
 @Entity
+@Table(name = "users")
 public class User
 {
-//    private Long  id;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
-
-    //the jpa entity can have multiple constructors but
-    // the presence of default constructor is mendatory
-    //or use Lombok's NoArgsConstructor
-
-//    public User() {
-//    }
-//
-//    public User(Long id, String firstName, String lastName) {
-//        this.id = id;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//    }
 }
